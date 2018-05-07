96807dac6700101eeae6cc79cff36f5bd405bb6e
Hana LBATH <hana.lbath@insa-lyon.fr>
2018-05-07 14:44:21 +0200

    created a main in exe.cpp and testsHana.cpp

---
+++ exe.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include "Bacteria.h"
+#include "Cell.h"
+#include "Ga.h"
+#include "Gb.h"
+
+int main(int argc, char* argv[]){
+	std::cout << "Hello world!" << std::endl;
+	return 0;
+}
---
+++ testsHana.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include "Bacteria.h"
+#include "Cell.h"
+#include "Ga.h"
+#include "Gb.h"
+
+int main(int argc, char* argv[]){
+	std::cout << "Hello world!" << std::endl;
+	Gb bactB(1.0,2.0,3.0);
+	Ga bactA(1.0,2.0,3.0);
+	
+	return 0;
+}
