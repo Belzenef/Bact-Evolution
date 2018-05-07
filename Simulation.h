1b28adcd272e68d5778a6f60be691eec408bd143
Elise Jacquemet <elise.jacquemet@insa-lyon.fr>
2018-05-07 17:12:32 +0200

    Implemented class Simulation with declarations and empty definitions

--- Simulation.h
+++ Simulation.h
@@ -1,1 +1,36 @@
+#ifndef SIMUL_H  
+#define SIMUL_H
+
+#include "Grid.h"
+
+class Simulation {
+ public :
+// =====================================================================
+//                               Constructors
+// =====================================================================
+  Simulation(unsigned int height, unsigned int width, float ainit, 
+	float pdeath, float pmut, float Raa, float Rab, float Rbb, 
+	float Rbc, float d, float wmin, float t, float tend, float dt); 
+
+// =====================================================================
+//                                Destructor
+// =====================================================================
+  virtual ~Simulation();
+  
+// =====================================================================
+//                           Public Function members
+// =====================================================================
+	void run();
+
+ protected :
+// =====================================================================
+//                               Data members
+// =====================================================================
+  float t_;
+  float tend_;
+  float dt_;
+  Grid * grid;
+};
+
+#endif
 
