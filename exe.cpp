7bd1b26a06c456b5cb9d48f682038224ff61921c
Hana LBATH <hana.lbath@insa-lyon.fr>
2018-05-25 08:51:22 +0200

    completed exe.cpp

--- exe.cpp
+++ exe.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <fstream>
 #include "Bacteria.h"
 #include "Cell.h"
 #include "Ga.h"
@@ -6,11 +7,12 @@
 #include "Simulation.h"
 
 int main(int argc, char* argv[]){
-	std::cout << "Hello world!" << std::endl;
+
+	srand(3); //seed used by rand (	we'll have to use srand(time(NULL)) eventually)
 
 	//////// Defining T and Ainit domain ////////
 	unsigned int Tmin=1;
-	unsigned int Tmax=1500;
+	unsigned int Tmax=200;
 
 	float AinitMin=0.;
 	float AinitMax=50.;
@@ -23,15 +25,24 @@ int main(int argc, char* argv[]){
 	unsigned int T=Tmin;
 	unsigned int ainit=AinitMin;
 
-	//////// creating  ////////
-	while(T<Tmax){
-		while(ainit<AinitMax){
+	//////// Creating the output file ////////
+	ofstream myfile;
+	myfile.open("SimulationOutputs.csv");
+
+	//////// Filling the output file with the simulation  ////////
+	while(T<=Tmax){
+		std::cout << "T = " << T << std::endl;
+		while(ainit<=AinitMax){
 			Simulation sim(32, 32, ainit, .02, 0., .1,.1,.1,.1, .1, .001, T, 5000, dt);
-			sim.run();
+			myfile << ainit <<","<<T<<","<<sim.run()<<"\n";
 			ainit+=dA;
 		}
 		++T;
+		ainit=AinitMin;
 	}
+
+	//////// Closing the output file ////////
+	myfile.close();
 
 	return 0;
 }
