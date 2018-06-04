b3b3b3dcf219a6b287d622cd6f3a801c1383357b
Elise Jacquemet <elise.jacquemet@insa-lyon.fr>
2018-06-04 10:27:14 +0200

    modifications légende

--- portraitPhase.R
+++ portraitPhase.R
@@ -12,19 +12,20 @@ exclusion<-subset(data,data$result==0)
 cohabitation<-subset(data,data$result>0)
 cohabitation <- cohabitation[order(cohabitation$result), ] 
 n <- as.numeric(length(cohabitation$T))
-#c <- 1
+c <- 1
+
 # Echelle de couleurs
+if(length(cohabitation)!=0){
 color=heat_hcl(length(cohabitation$result))[rank(cohabitation$result)]
+}
 
 # Affichage
 par(mar=c(5.1,4.1,4.1,8.1), xpd=TRUE)
-plot(extinction$ainit,extinction$T, col='black', pch=15, 
-     xlim=c(min(data$ainit),max(data$ainit)), ylim=c(min(data$T),max(data$T)))
-points(exclusion$ainit,exclusion$T, col="#990033", pch=15)
-points(cohabitation$ainit,cohabitation$T, col=color, pch=15)
-legend("topright",title="S/L",inset=c(-0.15,0),pch=15,col=color,
-       legend=seq(min(cohabitation$result),max(cohabitation$result),length.out = length(color)))
-
-
-#hey <- ggplot(cohabitation, aes(x=cohabitation$ainit, y=cohabitation$T, color=cohabitation$result))
-#hey + geom_point(size=6) + scale_color_gradient(low="#D33F6A", high="#E2E6BD")
+plot(extinction$ainit,extinction$T, col='black', pch=15, cex=c, las=1, main="Portrait de phase",
+     xlim=c(min(data$ainit),max(data$ainit)), ylim=c(min(data$T),max(data$T)),
+     xlab="ainit", ylab="T")
+points(exclusion$ainit,exclusion$T, col="#990033", pch=15, cex=c)
+points(cohabitation$ainit,cohabitation$T, col=color, pch=15, cex=c)
+legend("topright",title="S/L",inset=c(-0.13,0),pch=15,col=cbind(color,c("black","#990033")),
+       legend=cbind(seq(min(cohabitation$result),max(cohabitation$result),
+                        length.out = length(color)),c("extinction","exclusion")),bty="n")
