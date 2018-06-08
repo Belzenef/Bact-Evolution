4d50bd7a267d3596d3caeaa4e7c34948a5ab251c
Belzenef <33092663+Belzenef@users.noreply.github.com>
2018-06-08 14:39:40 +0200

    Add files via upload

--- portraitPhase.R
+++ portraitPhase.R
@@ -21,11 +21,11 @@ color=heat_hcl(length(cohabitation$result))[rank(cohabitation$result)]
 
 # Affichage
 par(mar=c(5.1,4.1,4.1,8.1), xpd=TRUE)
-plot(extinction$ainit,extinction$T, col='black', pch=15, cex=c, las=1, main="Portrait de phase",
+plot(extinction$ainit,extinction$T, col='black', pch=15, cex=c, las=1, main="Portrait de phase - pmut=0.001",
      xlim=c(min(data$ainit),max(data$ainit)), ylim=c(min(data$T),max(data$T)),
      xlab="ainit", ylab="T")
 points(exclusion$ainit,exclusion$T, col="#990033", pch=15, cex=c)
 points(cohabitation$ainit,cohabitation$T, col=color, pch=15, cex=c)
-legend("topright",title="S/L",inset=c(-0.13,0),pch=15,col=cbind(color,c("black","#990033")),
+legend("topright",title="S/L",inset=c(-0.2,0),pch=15,col=cbind(c(color[1],color[n]),c("black","#990033")),
        legend=cbind(seq(min(cohabitation$result),max(cohabitation$result),
-                        length.out = length(color)),c("extinction","exclusion")),bty="n")
+                        length.out = 2),c("extinction","exclusion")),bty="n")
