0baca08c7adfd0db9ddc8e82cc7edc8ce01d36f4
Elise Jacquemet <elise.jacquemet@insa-lyon.fr>
2018-06-12 18:07:59 +0200

    version finale

--- portraitPhase.R
+++ portraitPhase.R
@@ -3,7 +3,7 @@ library(colorspace)
 
 
 # | ainit | T | S/L |
-data<-read.table("SimulationOutputs.csv", header = FALSE, sep = ",", dec=".")
+data<-read.table("SimulationOutputspmut0.csv", header = FALSE, sep = ",", dec=".")
 names(data)<-c("ainit","T","result")
 
 # Séparation des différents états du reste des données
@@ -12,7 +12,7 @@ exclusion<-subset(data,data$result==0)
 cohabitation<-subset(data,data$result>0)
 cohabitation <- cohabitation[order(cohabitation$result), ] 
 n <- as.numeric(length(cohabitation$T))
-c <- 1
+c <-1
 
 # Echelle de couleurs
 if(length(cohabitation)!=0){
@@ -29,3 +29,32 @@ points(cohabitation$ainit,cohabitation$T, col=color, pch=15, cex=c)
 legend("topright",title="S/L",inset=c(-0.13,0),pch=15,col=cbind(color,c("black","#990033")),
        legend=cbind(seq(min(cohabitation$result),max(cohabitation$result),
                         length.out = length(color)),c("extinction","exclusion")),bty="n")
+
+# | ainit | T | S/L |
+data<-read.table("SimulationOutputspmut0001.csv", header = FALSE, sep = ",", dec=".")
+names(data)<-c("ainit","T","result")
+
+# Séparation des différents états du reste des données
+extinction<-subset(data,data$result==-1)
+exclusion<-subset(data,data$result==0)
+cohabitation<-subset(data,data$result>0)
+cohabitation <- cohabitation[order(cohabitation$result), ] 
+n <- as.numeric(length(cohabitation$T))
+c <-1
+
+# Echelle de couleurs
+if(length(cohabitation)!=0){
+  color=heat_hcl(length(cohabitation$result))[rank(cohabitation$result)]
+}
+
+# Affichage
+par(mar=c(5.1,4.1,4.1,8.1), xpd=TRUE)
+plot(extinction$ainit,extinction$T, col='black', pch=15, cex=c, las=1, main="Portrait de phase - pmut=0.001",
+     xlim=c(min(data$ainit),max(data$ainit)), ylim=c(min(data$T),max(data$T)),
+     xlab="ainit", ylab="T")
+points(exclusion$ainit,exclusion$T, col="#990033", pch=15, cex=c)
+points(cohabitation$ainit,cohabitation$T, col=color, pch=15, cex=c)
+legend("topright",title="S/L",inset=c(-0.13,0),pch=15,col=cbind(c(color[1],color[n]),c("black","#990033")),
+       legend=cbind(seq(min(cohabitation$result),max(cohabitation$result),
+                        length.out = 2),c("extinction","exclusion")),bty="n")
+
